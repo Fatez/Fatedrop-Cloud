ffbc9a60e013e51f5191e6db6ab5503358956ca9
Fatez <cdotkeeley@gmail.com>
2026-08-29 02:15:09 +0100

    Remove redundant Local Radar bootstrap preloader

--- signal-engine/src/encounters/operator-local-radar-bootstrap.mjs
+++
@@ -1,5 +0,0 @@
-import { createStore } from "../stores/index.mjs";
-import { startOperatorLocalRadarIntake } from "./operator-local-radar-intake.mjs";
-
-const store = createStore();
-startOperatorLocalRadarIntake({ store });
